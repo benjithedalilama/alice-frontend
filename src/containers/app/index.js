59ffe78b81d2cf14387442759a0bce89d0b0e5a1
benjithedalilama <benjithedalillama@gmail.com>
2018-10-03 18:02:59 +0100

    added home path

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -6,12 +6,12 @@ import About from '../about'
 const App = () => (
   <div>
     <header>
-      <Link to="/">Home</Link>
+      <Link to="/home">Home</Link>
       <Link to="/about-us">About</Link>
     </header>
 
     <main>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/home" component={Home} />
       <Route exact path="/about-us" component={About} />
     </main>
   </div>
