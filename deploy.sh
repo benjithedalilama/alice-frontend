61cf258b50303ab9968eae1404e7ae19499e8447
benjithedalilama <benjithedalillama@gmail.com>
2018-10-07 19:01:30 +0100

    running hyper in detached mode to not get output

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ IMAGE="benjithedalilama/alice-frontend:MVP"
 
 ./hyper pull $IMAGE
 ./hyper compose down -p frontend
-./hyper compose up -p frontend -f hyper-compose.yml
+./hyper compose up -p frontend -f hyper-compose.yml -d
