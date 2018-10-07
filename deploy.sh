9fef28b1ad4836f788dd4921ac9c12b8dbadd214
benjithedalilama <benjithedalillama@gmail.com>
2018-10-07 18:48:45 +0100

    attempting my first actual deployment from Circle CI

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 IMAGE="benjithedalilama/alice-frontend:MVP"
 
 ./hyper pull $IMAGE
-# ./hyper compose down -p frontend
-# ./hyper compose up -p frontend -f hyper-compose.yml
+./hyper compose down -p frontend
+./hyper compose up -p frontend -f hyper-compose.yml
