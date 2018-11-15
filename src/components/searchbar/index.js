7d49dbaff0eae194086352c55b7d32d60ffdefd5
benjithedalilama <benjithedalillama@gmail.com>
2018-11-15 11:15:19 +0000

    added fontawesome to project, cleaned up searchbar

--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './searchbar.css'
 
 const Searchbar = props => (
   <div className={props.className + ' searchbar'}>
       <input className="searchbar__input" placeholder="Search"></input>
-      <div className="searchbar__icon"></div>
+      <div className="searchbar__searchButton">
+        <FontAwesomeIcon className="searchbar__icon" icon={faSearch} />
+      </div>
   </div>
 )
 
