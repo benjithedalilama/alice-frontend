f34d4ab379f437573b58bb21b998e4593f98c676
benjithedalilama <benjithedalillama@gmail.com>
2018-11-23 14:28:10 +0000

    added button to navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { push } from 'connected-react-router'
 import Breadcrumbs from '../breadcrumbs'
 import Searchbar from '../searchbar'
 import ProfilePreview from '../profilepreview'
@@ -9,14 +10,18 @@ import './navbar.css'
 const Navbar = props => (
   <div className="navbar">
     <Breadcrumbs className="navbar__element"></Breadcrumbs>
-    <Searchbar className="navbar__element"></Searchbar>
-    <ProfilePreview className="navbar__element"></ProfilePreview>
+    <Searchbar className="navbar__element navbar__element--centered"></Searchbar>
+    <ProfilePreview className="navbar__element">
+      <button onClick={() => props.changePage('/add-hub')} className="button button--alt">ADD HUB</button>
+    </ProfilePreview>
   </div>
 )
 
 const mapStateToProps = ({ counter }) => ({})
 
-const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({
+  changePage: (destination) => push(destination)
+}, dispatch)
 
 export default connect(
   mapStateToProps,
--- src/components/profilepreview/index.js
+++ src/components/profilepreview/index.js
@@ -5,6 +5,7 @@ import './profilepreview.css'
 
 const ProfilePreview = props => (
   <div className={props.className + ' profilePreview'}>
+    {props.children}
     <p className="profilePreview__text">USER.EMAIL</p>
     <img className="profilePreview__image" src="https://lh3.googleusercontent.com/--SUy6eYwjwU/AAAAAAAAAAI/AAAAAAAAAAA/AGDgw-jKi24cBTO8NdPN3lLZeA2nUDGQ6g/s48-c-mo/photo.jpg" alt="Profile" />
   </div>
