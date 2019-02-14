6c7226d7bfdd92eb9c4834c2cc81e7d61939c94f
benjithedalilama <benjithedalillama@gmail.com>
2019-02-14 01:02:25 -0800

    *Lessons were learned*, removed dev workaround from authentication

--- src/components/protectedroute/index.js
+++ src/components/protectedroute/index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
 const ProtectedRoute = ({ isAuthenticated, ...props }) => (
-  // Fix this, workaround for dev
-  true
+  isAuthenticated
   ? <Route {...props}/>
   : <Redirect to="/login"/>
 )
